06e58f39dc2522be78c3320da771538e6960c19a
YopKiller <shimizudofus@gmail.com>
2026-07-01 09:18:36 +0200

    Updates

---
+++ assets/esm-DdhOjW4P.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DuhfWYsC.js","assets/index-B_Cd8QSt.js","assets/index-DkRPXpqg.css"])))=>i.map(i=>d[i]);
+import{r as e,t}from"./index-B_Cd8QSt.js";var n;(function(e){e.Heavy=`HEAVY`,e.Medium=`MEDIUM`,e.Light=`LIGHT`})(n||={});var r;(function(e){e.Success=`SUCCESS`,e.Warning=`WARNING`,e.Error=`ERROR`})(r||={});var i=e(`Haptics`,{web:()=>t(()=>import(`./web-DuhfWYsC.js`).then(e=>new e.HapticsWeb),__vite__mapDeps([0,1,2]))});export{i as Haptics,n as ImpactStyle,r as NotificationType};
